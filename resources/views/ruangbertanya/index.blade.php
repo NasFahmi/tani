0d23a0571423f3f7ba71bec8d6c76b07f8c53d57
NasFahmi <75nasrulfahmi@gmail.com>
2024-06-30 13:16:07 +0700

    add btn back

--- resources/views/ruangbertanya/index.blade.php
+++ resources/views/ruangbertanya/index.blade.php
@@ -3,10 +3,30 @@
 @section('content')
     <div class="mx-auto">
         <div class="flex items-center justify-center mb-4">
-            <h1 class="text-2xl font-extrabold text-black font-post-no-bills-jaffna">HamaPetik</h1>
-            <div class="ml-2">
-                <img src="{{ asset('asset/icon/image_logo.png') }}" alt="Plant" class="object-cover w-10 h-10 rounded-full">
+            <div class="flex items-center justify-between w-full mt-5">
+                <a href="{{ route('home.index') }}" class="flex items-center">
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" class="w-6 h-6">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M16.1795 3.26875C15.7889 2.87823 15.1558 2.87823 14.7652 3.26875L8.12078 9.91322C6.94952 11.0845 6.94916 12.9833 8.11996 14.155L14.6903 20.7304C15.0808 21.121 15.714 21.121 16.1045 20.7304C16.495 20.3399 16.495 19.7067 16.1045 19.3162L9.53246 12.7442C9.14194 12.3536 9.14194 11.7205 9.53246 11.33L16.1795 4.68297C16.57 4.29244 16.57 3.65928 16.1795 3.26875Z"
+                                fill="#000000"></path>
+                        </g>
+                    </svg>
+                    <h1 class="ml-2 font-medium text-black">Back</h1>
+                </a>
+                <div class="flex ">
+                    <h1 class="text-2xl font-extrabold text-black font-post-no-bills-jaffna">HamaPetik</h1>
+                    <div class="ml-2">
+                        <img src="{{ asset('asset/icon/image_logo.png') }}" alt="Plant"
+                            class="object-cover w-10 h-10 rounded-full">
+                    </div>
+                </div>
+
+                <div class="w-[65px] h-6"></div>
             </div>
+
         </div>
         <hr class="mb-4 border-gray-300">
         <div class="text-center">
@@ -18,11 +38,15 @@
             @foreach ($messages as $message)
                 @if ($message->sender == 'user')
                     <div class="mb-2 text-right">
-                        <p class="inline-block px-4 py-2 text-sm text-white bg-blue-500 rounded-lg max-w-80 message-content">{{ $message->content }}</p>
+                        <p
+                            class="inline-block px-4 py-2 text-sm text-white bg-blue-500 rounded-lg max-w-80 message-content">
+                            {{ $message->content }}</p>
                     </div>
                 @else
                     <div class="mb-2">
-                        <p class="inline-block px-4 py-2 text-sm text-gray-700 bg-gray-200 rounded-lg max-w-80 message-content">{{ $message->content }}</p>
+                        <p
+                            class="inline-block px-4 py-2 text-sm text-gray-700 bg-gray-200 rounded-lg max-w-80 message-content">
+                            {{ $message->content }}</p>
                     </div>
                 @endif
             @endforeach
@@ -38,7 +62,8 @@
     </div>
     <form id="chat-form">
         <div class="flex items-center p-4 mb-4 bg-white rounded-lg shadow-md" style="border-radius: 28px;">
-            <input type="text" id="message" class="flex-grow p-2 bg-transparent border border-gray-300 rounded-md" placeholder="Tulis pesan...">
+            <input type="text" id="message" class="flex-grow p-2 bg-transparent border border-gray-300 rounded-md"
+                placeholder="Tulis pesan...">
             <button type="submit" class="flex items-center justify-center w-12 h-12 ml-4 bg-green-600 rounded-full">
                 <img src="https://img.icons8.com/ios-filled/50/ffffff/paper-plane.png" alt="Send" class="w-6 h-6">
             </button>
